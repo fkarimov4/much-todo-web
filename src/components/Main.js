a69e5177b1c7c12e74f10b26ae48c2c748706bc4
Farrukh Karimov <fkarimov4@gmail.com>
2022-02-18 10:58:02 -0500

    connected app to Firestore

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import TaskList from "./TaskList"
+
+export default function Main() {
+    return (
+        <section>
+            <h1>Much Todo</h1>
+            <TaskList/>
+        </section>
+    )
+}
---
+++ src/components/TaskList.js
@@ -0,0 +1,28 @@
+import { List } from "antd";
+import { useEffect, useState } from "react";
+import Task from "./Task";
+
+const fakeTasks = [
+  { id: 1, task: "Buy Milk", done: false },
+  { id: 2, task: "Brew Beer", done: false },
+  { id: 3, task: "Buy Trulys", done: true },
+  { id: 4, task: "Drink Wine", done: true },
+  { id: 5, task: "Buy Paper Towels", done: false },
+];
+
+export default function TaskList() {
+  const [tasks, setTasks] = useState(fakeTasks);
+  useEffect(() => {
+      fetch('https://much-todo-fk.uc.r.appspot.com/tasks')
+      .then(response => response.json())
+      .then(setTasks)
+      .catch()
+  })
+  return (
+    <List
+      bordered
+      dataSource={tasks}
+      renderItem={(item) => <Task item={item} />}
+    />
+  );
+}
